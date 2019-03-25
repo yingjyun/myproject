7d037da9adee83ec42602e8e0b3a5976cae04884
yingjyun <43268589+yingjyun@users.noreply.github.com>
2019-03-25 15:09:16 +0800

    Initial commit

---
+++ src/main/kotlin/com/lab626/Hello.kt
@@ -0,0 +1,14 @@
+package com.lab626
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin")
+    }
+}
