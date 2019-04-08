d9c5d705bee7652dcedd8cf1e8768638cbe3bfed
yingjyun <43268589+yingjyun@users.noreply.github.com>
2019-04-08 15:58:03 +0800

    Java and kotlin variable, data type

--- src/main/kotlin/com/lab626/Hello.kt
+++ src/main/kotlin/com/lab626/Hello.kt
@@ -5,6 +5,11 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age : Int = 25
+    age = age.plus(1)
+    val weight = 65.5
+    val enroll : Boolean = true
+    println(age)
 }
 
 class Human {
